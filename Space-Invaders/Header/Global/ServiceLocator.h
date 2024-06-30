1fb0f05d1017177880868cd4829b98a454943f84
Mirza_4565 <mirzaalishaik2000@gmail.com>
2024-07-01 04:35:27 +0530

    Enemy_Service

--- Space-Invaders/Header/Global/ServiceLocator.h
+++ Space-Invaders/Header/Global/ServiceLocator.h
@@ -4,6 +4,8 @@
 #include "../../Header/Player/PlayerService.h"
 #include "../../Header/Time/TimeService.h"
 #include "../../Header/UI/UIService.h"
+#include "../../Header/Enemy/EnemyService.h"
+
 
 
 // ServiceLocator Class Summary: This class manages access to various services in the application.
@@ -23,7 +25,11 @@ namespace Global {
         Player::PlayerService* player_service;
         // - time_service: Handles graphics-related tasks.
         Time::TimeService* time_service;
+        // - ui_service: Handles graphics-related tasks.
         UI::UIService* ui_service;
+        // - enemy_service: Handles graphics-related tasks.
+        Enemy::EnemyService* enemy_service;
+        
         // Private Constructor and Destructor:
 
         // Constructor for initializing the ServiceLocator.
@@ -48,7 +54,8 @@ namespace Global {
         Event::EventService* getEventService();   // Retrieve the EventService instance.
         Graphic::GraphicService* getGraphicService();   // Retrieve the GraphicService instance.
         Player::PlayerService* getPlayerService();    // Retrieve the PlayerService instance.
-        Time::TimeService* getTimeService();       //// Retrieve the TimeService instance.
-        UI::UIService* getUIService();
+        Time::TimeService* getTimeService();       // Retrieve the TimeService instance.
+        UI::UIService* getUIService();           // Retrieve the UIService instance.
+        Enemy::EnemyService* getEnemyServices(); // Retrieve the EnemyService instance.
     };
 }
--- Space-Invaders/Source/Global/ServiceLocator.cpp
+++ Space-Invaders/Source/Global/ServiceLocator.cpp
@@ -1,4 +1,5 @@
 #include "../../Header/Global/ServiceLocator.h"
+#include "../../Header/Main/GameService.h"
 
 namespace Global {
 
@@ -7,6 +8,8 @@ namespace Global {
 	using namespace Event;
 	using namespace Player;
 	using namespace UI;
+	using namespace Enemy;
+	using namespace Main;
 
 	// Constructor: Initializes the graphic_service pointer to null and creates services.
 	ServiceLocator::ServiceLocator() {
@@ -14,7 +17,8 @@ namespace Global {
 		event_service = nullptr; // Initialize event_service to null.
 		player_service = nullptr; //Initialize player_service to null.
 		time_service = nullptr;  //Initialize time_service to null.
-		ui_service = nullptr;
+		ui_service = nullptr;    //Initialize ui_service to null
+		enemy_service = nullptr; //Initialize enemy_service to null
 
 		createServices(); // Call createServices to instantiate services
 	}
@@ -30,7 +34,8 @@ namespace Global {
 		event_service = new EventService();// Dynamically create a EventService instance
 		player_service = new PlayerService();// Dynamically create a PlayerService instance
 		time_service = new TimeService(); // Dynamically create a TimeService instance
-		ui_service = new UIService();
+		ui_service = new UIService();    // Dynamically create a UIService instance
+		enemy_service = new EnemyService(); // Dynamically create a EnemyService instance
 	}
 
 	// Deletes allocated services to prevent memory leaks, specifically the graphic service.
@@ -39,7 +44,8 @@ namespace Global {
 		delete(event_service); // Delete the event_service instance
 		delete(player_service);  //Delete the player_service instance
 		delete(time_service);   //Delete the time_service instance
-		delete(ui_service);
+		delete(ui_service);    //Delete the ui_service instance
+		delete(enemy_service); //Delete the enemy_service instance
 	}
 
 	// Returns a pointer to ServiceLocator.
@@ -54,25 +60,52 @@ namespace Global {
 		event_service->initialize();// Initialize event service.
 		player_service->initialize();// Initialize player service.
 		time_service->initialize(); // Initialize time_service.
-		ui_service->initialize();
+		ui_service->initialize();  // Initialize ui_service.
+		enemy_service->initialize();
 
 	}
 
 	// Updates the state of the graphic service.
 	void ServiceLocator::update() {
 		graphic_service->update();// Update graphic service
-		event_service->update();// Update event service.
-		player_service->update();// Update player service
 		time_service->update(); // Update time service'
+		event_service->update();// Update event service.
+		//player_service->update();// Update player service
+		
+		//ui_service->update();  // Update ui service'
+		//enemy_service->update(); // Update enemy service'
+		if (GameService::getGameState() == GameState::GAMEPLAY)
+		{
+			player_service->update();
+			enemy_service->update();
+		}
+
 		ui_service->update();
+
 	}
 
-	// Renders using the graphic service.
-	void ServiceLocator::render() {
-		graphic_service->render(); // Render graphic service
-		player_service->render(); //Render Player service
+	void ServiceLocator::render()
+	{
+		graphic_service->render();
+		if (GameService::getGameState() == GameState::GAMEPLAY)
+		{
+			player_service->render();
+			enemy_service->render();
+		}
+
 		ui_service->render();
+
+
 	}
+	
+
+	// Renders using the graphic service.
+	//void ServiceLocator::render() {
+	//	graphic_service->render(); // Render graphic service
+	//	player_service->render(); //Render Player service
+	//	ui_service->render();   //Render UI service
+	//	enemy_service->render(); //Render Enemy service
+	//}
 
 	// Returns a pointer to the currently set graphic service.
 	GraphicService* ServiceLocator::getGraphicService() { return graphic_service; }
@@ -80,5 +113,6 @@ namespace Global {
 	PlayerService* ServiceLocator::getPlayerService() { return player_service; }
 	TimeService* ServiceLocator::getTimeService() { return time_service; }
 	UIService* ServiceLocator::getUIService() { return ui_service; }
+	EnemyService* ServiceLocator::getEnemyServices() { return enemy_service; }
 
 }
